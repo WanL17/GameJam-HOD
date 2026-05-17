7a19b0167745ea3111ddc66d159b264681132949
khalil almwakeh <khalil.almwakeh@epitech.eu>
2026-05-17 18:14:28 +0200

    Spider: 5min hunt timer, HUD countdown, player chase + game over

--- scenes/rooms/room_01.gd
+++ scenes/rooms/room_01.gd
@@ -4,8 +4,9 @@ extends Node3D
 
 func _ready():
 	RoomManager.init_room(3)
+	# Spider does the intro visit after 30s
 	get_tree().create_timer(30.0).timeout.connect(_spider_visit)
 
 func _spider_visit():
-	if not RoomManager.found_bugs >= RoomManager.total_bugs:
+	if RoomManager.found_bugs < RoomManager.total_bugs:
 		spider.trigger()
