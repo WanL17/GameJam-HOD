6a62605ddbbe48155fc762271f7bde71a3b82e76
Khalil Almwakeh <khalil.almwakeh@epitech.eu>
2026-05-17 16:59:38 +0200

    added new room

--- scenes/rooms/room_01.gd
+++ scenes/rooms/room_01.gd
@@ -1,4 +1,11 @@
 extends Node3D
 
+@onready var spider = $SpiderAI
+
 func _ready():
-	RoomManager.init_room(3)  # Change 3 to however many bugged assets you place
+	RoomManager.init_room(3)
+	get_tree().create_timer(30.0).timeout.connect(_spider_visit)
+
+func _spider_visit():
+	if not RoomManager.found_bugs >= RoomManager.total_bugs:
+		spider.trigger()
